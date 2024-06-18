6a043978049883d6072c7594df0fc349fd456f3f
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-18 07:35:06 +0530

    saveCustomer Added to ServiceImpl

---
+++ src/main/java/lk/ijse/gdse66/service/CustomerService.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse66.service;
+
+/**
+ * @author : Kavithma Thushal
+ * @project : SpringBoot-MySQL-CRUD
+ * @since : 7:24 AM - 6/18/2024
+ **/
+public interface CustomerService {
+    void saveCustomer();
+}
---
+++ src/main/java/lk/ijse/gdse66/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,20 @@
+package lk.ijse.gdse66.service.impl;
+
+import lk.ijse.gdse66.service.CustomerService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author : Kavithma Thushal
+ * @project : SpringBoot-MySQL-CRUD
+ * @since : 7:25 AM - 6/18/2024
+ **/
+@Service
+@Transactional
+public class CustomerServiceImpl implements CustomerService {
+
+    @Override
+    public void saveCustomer() {
+
+    }
+}
