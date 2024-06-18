d0e5b3e7bd41e1d4cd901512a6fb3077abe0be09
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-18 06:21:29 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/gdse66/springbootmysqlcrud/SpringBootMySqlCrudApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.springbootmysqlcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMySqlCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMySqlCrudApplication.class, args);
+    }
+
+}
