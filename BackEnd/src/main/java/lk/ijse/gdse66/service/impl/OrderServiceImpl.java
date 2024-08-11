1a2258127282c21e471e638b1fb5b71e2e707688
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-08-11 11:43:01 +0530

    Update Item Qty

--- BackEnd/src/main/java/lk/ijse/gdse66/service/impl/OrderServiceImpl.java
+++ BackEnd/src/main/java/lk/ijse/gdse66/service/impl/OrderServiceImpl.java
@@ -1,13 +1,18 @@
 package lk.ijse.gdse66.service.impl;
 
 import lk.ijse.gdse66.dto.OrdersDTO;
+import lk.ijse.gdse66.entity.Item;
+import lk.ijse.gdse66.entity.OrderDetails;
 import lk.ijse.gdse66.entity.Orders;
+import lk.ijse.gdse66.repo.ItemRepo;
 import lk.ijse.gdse66.repo.OrderRepo;
 import lk.ijse.gdse66.service.OrderService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 /**
  * @author : Kavithma Thushal
@@ -22,12 +27,23 @@ public class OrderServiceImpl implements OrderService {
     private OrderRepo orderRepo;
 
     @Autowired
+    private ItemRepo itemRepo;
+
+    @Autowired
     private ModelMapper modelMapper;
 
     @Override
     public void placeOrder(OrdersDTO ordersDTO) {
         Orders order = modelMapper.map(ordersDTO, Orders.class);
-        order.getOrderDetailsList().forEach(orderDetail -> orderDetail.setOrders(order));
+
+        for (OrderDetails orderDetail : order.getOrderDetailsList()) {
+            orderDetail.setOrders(order);
+
+            // Update Item Qty
+            Item item = itemRepo.findById(orderDetail.getItem().getCode()).orElse(null);
+            item.setQtyOnHand(item.getQtyOnHand() - orderDetail.getBuyQty());
+            itemRepo.save(item);
+        }
         orderRepo.save(order);
     }
 }
